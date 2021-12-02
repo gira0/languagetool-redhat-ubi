98d14b6938a979599c0f56decb92228d6d9cb239
gira <19488367+gira0@users.noreply.github.com>
2021-12-02 22:09:59 +0100

    Add public start option to make it available

--- startup.sh
+++ startup.sh
@@ -3,11 +3,11 @@
 DIR="/languagetool"
 if [ -d "$DIR" ]; then
   echo "${DIR} found! running with ngram data"
-  java -cp /opt/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --languageModel /languagetool --port 8080 --allow-origin '*'
+  java -cp /opt/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --languageModel /languagetool --port 8080 --allow-origin '*' --public
 
 else
   echo "${DIR} not found, running without ngram data"
-  java -cp /opt/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --port 8080 --allow-origin '*'
+  java -cp /opt/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --port 8080 --allow-origin '*' --public
 
 fi
 
