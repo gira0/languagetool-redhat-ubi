f7264b722db6283f64c75e0381e415296717fcc9
gira <19488367+gira0@users.noreply.github.com>
2021-12-02 21:45:15 +0100

    Change java path to make it automatically find it

--- startup.sh
+++ startup.sh
@@ -3,11 +3,11 @@
 DIR="/languagetool"
 if [ -d "$DIR" ]; then
   echo "${DIR} found! running with ngram data"
-  /usr/bin/java -cp /opt/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --languageModel /languagetool --port 8080 --allow-origin '*'
+  java -cp /opt/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --languageModel /languagetool --port 8080 --allow-origin '*'
 
 else
   echo "${DIR} not found, running without ngram data"
-  /usr/bin/java -cp /opt/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --port 8080 --allow-origin '*'
+  java -cp /opt/languagetool/languagetool-server.jar org.languagetool.server.HTTPServer --port 8080 --allow-origin '*'
 
 fi
 
