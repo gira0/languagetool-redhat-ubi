95f69529734dbcf91c06eb8fbf615b2d0c9e3f59
gira <19488367+gira0@users.noreply.github.com>
2024-05-04 00:39:22 +0200

    Update to new GITHUB_OUTPUT var

--- get_current_lt.sh
+++ get_current_lt.sh
@@ -2,4 +2,4 @@
 
 # This script retrieves the current version of LanguageTool from the Containerfile and sets the variable for the workflow.
 CONTAINER_LT=$(sed -E 's|ARG LT_VER=(.*)$|\1|g' Containerfile)
-echo "::set-output name=LT_VERSION::$CONTAINER_LT"
+echo "LT_VERSION=$CONTAINER_LT" >> "$GITHUB_OUTPUT"
--- get_latest_lt.sh
+++ get_latest_lt.sh
@@ -5,5 +5,5 @@ regex='\"name\": \"([0-9v.]{1,6})\"'
 
 if [[ $data =~ $regex ]]
 then
-  echo "::set-output name=LT_VERSION::${BASH_REMATCH[1]:1}"
+  echo "LT_VERSION=${BASH_REMATCH[1]:1}" >> "$GITHUB_OUTPUT"
 fi
