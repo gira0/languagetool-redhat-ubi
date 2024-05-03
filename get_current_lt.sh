615abef51e425f8d77f4f99f723f9cfe39c463b4
gira <19488367+gira0@users.noreply.github.com>
2024-05-04 00:43:14 +0200

    Fix sed output

--- get_current_lt.sh
+++ get_current_lt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This script retrieves the current version of LanguageTool from the Containerfile and sets the variable for the workflow.
-CONTAINER_LT=$(sed -E 's|ARG LT_VER=(.*)$|\1|g' Containerfile)
+CONTAINER_LT=$(sed -n -E 's|ARG LT_VER=(.*)$|\1|p' Containerfile)
 echo "LT_VERSION=$CONTAINER_LT" >> "$GITHUB_OUTPUT"
