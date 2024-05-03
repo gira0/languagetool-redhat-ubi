bc442112d3a8217ee8e5624501d8fe8d0bb7d3e1
gira <19488367+gira0@users.noreply.github.com>
2024-05-03 23:07:29 +0200

    Enhance healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,13 +2,30 @@
 #--interval=2m --start-period=5m --timeout=5s CMD curl -f http://localhost:8080/v2/languages || exit 1
 
 # Run the podman container
-container_id=$(podman run -d --rm -p 8080:8080 $1)
+container_id=$(podman run -d --rm -p 8080:8080 "$1")
 
 # Wait for the container to start
 sleep 5
 
 # Test the container via a curl API call
-curl -f http://localhost:8080/v2/languages || exit 1
+status_code=$(curl -o response_body.json -w "%{http_code}" http://localhost:8080/v2/languages)
+
+echo "Status code: $status_code"
+
+if [ "$status_code" -ne 200 ]; then
+  echo "Failed to get a 200 status code"
+  exit 1
+fi
+
+echo "Response body:"
+cat response_body.json
+
+COUNT=$(jq '. | length' response_body.json)
+
+if [ "$COUNT" -gt 50 ]; then
+  echo "Failed to get a response body"
+  exit 1
+fi
 
 # Stop the container
-podman stop $container_id
+podman stop "$container_id"
